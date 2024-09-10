0434afd577cbbce4195a42fe02f211ea2c4374f2
Pedro Gabriel <pedro98gabriel@gmail.com>
2024-09-10 18:57:26 -0300

    Configuration to use View Binding

--- app/src/main/java/com/cantina/iflanche/MainActivity.kt
+++ app/src/main/java/com/cantina/iflanche/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.cantina.iflanche
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.cantina.iflanche.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
