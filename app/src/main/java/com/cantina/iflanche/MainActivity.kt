10fed93c5ba6ba333ea7a6f6d27011a43ec6c283
Pedro Gabriel <pedro98gabriel@gmail.com>
2024-09-13 17:58:48 -0300

    login screen finished

--- app/src/main/java/com/cantina/iflanche/MainActivity.kt
+++ app/src/main/java/com/cantina/iflanche/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding  = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
     }
 }
