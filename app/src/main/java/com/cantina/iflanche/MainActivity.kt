c00b015d8dacabbaca02cfafa6478398dba376a2
Pedro Gabriel <pedro98gabriel@gmail.com>
2024-09-13 20:30:37 -0300

    adjustment in indentation

--- app/src/main/java/com/cantina/iflanche/MainActivity.kt
+++ app/src/main/java/com/cantina/iflanche/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.cantina.iflanche
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.cantina.iflanche.databinding.ActivityMainBinding
@@ -8,7 +7,7 @@ import com.cantina.iflanche.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding  = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.hide()
