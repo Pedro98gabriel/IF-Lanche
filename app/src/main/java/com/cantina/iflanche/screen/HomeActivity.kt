5d7616100c1aa8a91c25701d783a18e485bc208c
Pedro Gabriel <pedro98gabriel@gmail.com>
2024-11-11 23:47:13 -0300

    Update HomeActivity.kt

--- app/src/main/java/com/cantina/iflanche/screen/HomeActivity.kt
+++ app/src/main/java/com/cantina/iflanche/screen/HomeActivity.kt
@@ -1,8 +1,11 @@
 package com.cantina.iflanche.screen
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.cantina.iflanche.R
 import com.cantina.iflanche.databinding.ActivityHomeBinding
 
@@ -10,26 +13,28 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var drawerToggle: ActionBarDrawerToggle
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val userType = intent.getStringExtra("userType") ?: "Aluno"
-        setSupportActionBar(binding.toolbar)
+        setAppBarTitle("IF-Lanche")
+
+        val toolbar: Toolbar = binding.toolbar
+
+        appBarNotificationIcon()
 
         drawerToggle = ActionBarDrawerToggle(
             this,
             binding.drawerLayout,
-            binding.toolbar,
+            toolbar,
             R.string.drawer_open,
             R.string.drawer_close
         )
         binding.drawerLayout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
 
-        //configureAppBar(userType)
+        val userType = intent.getStringExtra("userType") ?: "Aluno"
 
 //        if (userType == "Administrador") {
 //            // Mostrar elementos específicos para administradores
@@ -40,5 +45,17 @@ class HomeActivity : AppCompatActivity() {
 //            binding.testUserType.text = "Sou um usuario comum"
 //        }
 
+    }
+
+    fun setAppBarTitle(title: String) {
+        val appBarText = binding.toolbarTitle
+        appBarText.text = title
+    }
+
+    private fun appBarNotificationIcon() {
+        val notificationIconAppBar: ImageView = binding.endIconNotification
+        notificationIconAppBar.setOnClickListener {
+            Toast.makeText(this, "Não implementado", Toast.LENGTH_SHORT).show()
+        }
     }
 }
