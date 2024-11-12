94458a2ada516cd515a60351723d6225413f4b6d
Pedro Gabriel <pedro98gabriel@gmail.com>
2024-11-12 10:22:36 -0300

    Showing specific drawer for each type of user(admin ou student)

--- app/src/main/java/com/cantina/iflanche/screen/HomeActivity.kt
+++ app/src/main/java/com/cantina/iflanche/screen/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.cantina.iflanche.screen
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -8,10 +9,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.cantina.iflanche.R
 import com.cantina.iflanche.databinding.ActivityHomeBinding
-import com.cantina.iflanche.screen.fragments.CategoryFragment
+import com.cantina.iflanche.screen.fragments.admin.CategoryFragment
 import com.cantina.iflanche.screen.fragments.HomeFragment
-import com.cantina.iflanche.screen.fragments.ProductFragment
-import com.cantina.iflanche.screen.fragments.SubCaregoryFragment
+import com.cantina.iflanche.screen.fragments.admin.ProductFragment
+import com.cantina.iflanche.screen.fragments.admin.SubCaregoryFragment
+import com.cantina.iflanche.screen.fragments.student.ConfigStudentFragment
+import com.cantina.iflanche.screen.fragments.student.ProfileStudentFragment
 import com.google.android.material.navigation.NavigationView
 
 
@@ -24,48 +27,30 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setAppBarTitle("IF-Lanche")
+        val userType = intent.getStringExtra("userType") ?: "Aluno"
+        if (userType == "Aluno") {
+            Toast.makeText(this, "Logado como aluno", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Logado como Administrador", Toast.LENGTH_SHORT).show()
+        }
 
         val toolbar: Toolbar = binding.toolbar
+
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        if (userType == "Aluno") {
+            navigationView.inflateMenu(R.menu.nav_menu_student)
+        } else {
+            navigationView.inflateMenu(R.menu.nav_menu_admin)
+        }
+
         navigationView.setNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_home -> {
-                    setAppBarTitle("IF-Lanche")
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, HomeFragment())
-                        .commit()
-                }
 
-                R.id.nav_product -> {
-                    setAppBarTitle("Cadastrar Produto")
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, ProductFragment())
-                        .commit()
-                }
-
-                R.id.nav_category -> {
-                    setAppBarTitle("Cadastrar Categoria")
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, CategoryFragment())
-                        .commit()
-                }
-
-                R.id.nav_subcategory -> {
-                    setAppBarTitle("Cadastrar Subcategoria")
-
-                    supportFragmentManager.beginTransaction()
-                        .replace(
-                            R.id.fragment_container,
-                            SubCaregoryFragment()
-                        )
-                        .commit()
-                }
-
-                R.id.nav_exit -> {
-                    Toast.makeText(this, "Não implementado", Toast.LENGTH_SHORT).show()
-                }
+            if (userType == "Aluno") {
+                drawerMenuItemStudent(item)
+            } else {
+                drawerMenuItemAdmin(item)
             }
+
             binding.drawerLayout.closeDrawers()
             true
         }
@@ -82,25 +67,87 @@ class HomeActivity : AppCompatActivity() {
         binding.drawerLayout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
 
-
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, HomeFragment()).commit()
-            navigationView.setCheckedItem(R.id.nav_home)
+            if (userType == "Aluno") {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, HomeFragment()).commit()
+                navigationView.setCheckedItem(R.id.nav_home_student)
+            } else {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, HomeFragment()).commit()
+                navigationView.setCheckedItem(R.id.nav_home_admin)
+            }
         }
 
+    }
 
-        val userType = intent.getStringExtra("userType") ?: "Aluno"
+    private fun drawerMenuItemAdmin(item: MenuItem) {
+        when (item.itemId) {
+            R.id.nav_home_admin -> {
+                setAppBarTitle("IF-Lanche")
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, HomeFragment())
+                    .commit()
+            }
 
-//        if (userType == "Administrador") {
-//            // Mostrar elementos específicos para administradores
-//            binding.testUserType.text = "Sou um administrador"
-//        } else {
-//            // Ocultar elementos específicos para administradores
-//
-//            binding.testUserType.text = "Sou um usuario comum"
-//        }
+            R.id.nav_product_admin -> {
+                setAppBarTitle("Cadastrar Produto")
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, ProductFragment())
+                    .commit()
+            }
 
+            R.id.nav_category_admin -> {
+                setAppBarTitle("Cadastrar Categoria")
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, CategoryFragment())
+                    .commit()
+            }
+
+            R.id.nav_subcategory_admin -> {
+                setAppBarTitle("Cadastrar Subcategoria")
+
+                supportFragmentManager.beginTransaction()
+                    .replace(
+                        R.id.fragment_container,
+                        SubCaregoryFragment()
+                    )
+                    .commit()
+            }
+
+            R.id.nav_exit_admin -> {
+                Toast.makeText(this, "Não implementado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun drawerMenuItemStudent(item: MenuItem) {
+        when (item.itemId) {
+            R.id.nav_home_student -> {
+                setAppBarTitle("IF-Lanche")
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, HomeFragment())
+                    .commit()
+            }
+
+            R.id.nav_profile_student -> {
+                setAppBarTitle("Perfil de Usuário")
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, ProfileStudentFragment())
+                    .commit()
+            }
+
+            R.id.nav_config_student -> {
+                setAppBarTitle("Configurações")
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, ConfigStudentFragment())
+                    .commit()
+            }
+
+            R.id.nav_exit_student -> {
+                Toast.makeText(this, "Não implementado", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     fun setAppBarTitle(title: String) {
