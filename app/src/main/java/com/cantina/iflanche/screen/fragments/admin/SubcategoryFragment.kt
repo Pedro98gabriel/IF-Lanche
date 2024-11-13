01f9e24e881fd2a3a3235ff0f21567b10b2b1ab1
Pedro Gabriel <pedro98gabriel@gmail.com>
2024-11-12 21:00:59 -0300

    Renamed file

--- app/src/main/java/com/cantina/iflanche/screen/fragments/admin/SubcategoryFragment.kt
+++ app/src/main/java/com/cantina/iflanche/screen/fragments/admin/SubcategoryFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SubCaregoryFragment.newInstance] factory method to
+ * Use the [SubcategoryFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SubCaregoryFragment : Fragment() {
+class SubcategoryFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -50,7 +50,7 @@ class SubCaregoryFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SubCaregoryFragment().apply {
+            SubcategoryFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
