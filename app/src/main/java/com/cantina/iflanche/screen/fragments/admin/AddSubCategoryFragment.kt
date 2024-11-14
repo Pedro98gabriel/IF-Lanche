9434fb95a096eacc07e67cc09b8dbef4ea8a81f0
Pedro Gabriel <pedro98gabriel@gmail.com>
2024-11-14 14:22:32 -0300

    editing and adding sub category

--- app/src/main/java/com/cantina/iflanche/screen/fragments/admin/AddSubCategoryFragment.kt
+++ app/src/main/java/com/cantina/iflanche/screen/fragments/admin/AddSubCategoryFragment.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.cantina.iflanche.databinding.FragmentAddSubCategoryBinding
+import com.cantina.iflanche.screen.HomeActivity
+import com.cantina.iflanche.utils.CommonFunctions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class AddSubCategoryFragment : Fragment() {
     private var _binding: FragmentAddSubCategoryBinding? = null
     private val binding get() = _binding!!
+    private lateinit var database: DatabaseReference
     private var subCategoryName: String? = null // Nome da categoria a ser editada
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        database = FirebaseDatabase.getInstance().reference.child("subcategories")
         subCategoryName = arguments?.getString("subCategoryName")
 
     }
@@ -26,8 +37,37 @@ class AddSubCategoryFragment : Fragment() {
     ): View? {
         _binding = FragmentAddSubCategoryBinding.inflate(inflater, container, false)
         val view = binding.root
+        CommonFunctions.addTextWatcherAndFocusListener(
+            listOf(
+                binding.tfAddSubCategoryContent to binding.tfAddSubCategory,
+            )
+        )
 
-        binding.tfAddSubCategoryContent.setText(subCategoryName)  // Preenche o campo com o nome da categoria
+        if (subCategoryName != null) {
+            (activity as? HomeActivity)?.setAppBarTitle("Editar Subcategoria")
+            binding.tfAddSubCategoryContent.setText(subCategoryName)  // Preenche o campo com o nome da categoria
+            binding.btnSaveSubCategory.text = "Atualizar"
+        } else {
+            (activity as? HomeActivity)?.setAppBarTitle("Adicionar Subcategoria")
+        }
+
+        binding.btnSaveSubCategory.setOnClickListener {
+            binding.progressBarAddSubCategory.visibility = ProgressBar.VISIBLE
+            val newCategoryName = binding.tfAddSubCategoryContent.text.toString().trim()
+
+            if (newCategoryName.isNotEmpty()) {
+                if (subCategoryName != null) {
+                    // Se estamos editando, atualiza a categoria no Firebase
+                    updateSubCategoryInFirebase(newCategoryName)
+                } else {
+                    // Se estamos criando, verifica se já existe e salva a nova categoria
+                    checkSubCategoryExists(newCategoryName)
+                }
+            } else {
+                binding.tfAddSubCategory.error = "Por favor, insira um nome de categoria"
+                binding.progressBarAddSubCategory.visibility = ProgressBar.GONE
+            }
+        }
 
         return view
     }
@@ -36,6 +76,125 @@ class AddSubCategoryFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun checkSubCategoryExists(subCategoryName: String) {
+        database.orderByValue().equalTo(subCategoryName)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        binding.tfAddSubCategory.error = "Categoria já existe"
+                        binding.progressBarAddSubCategory.visibility = ProgressBar.GONE
+                    } else {
+                        saveSubCategoryToFirebase(subCategoryName)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(
+                        activity,
+                        "Erro ao verificar categoria. Tente novamente.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    binding.progressBarAddSubCategory.visibility = ProgressBar.GONE
+                }
+            })
+    }
+
+
+    private fun saveSubCategoryToFirebase(subCategoryName: String) {
+        val lowerCaseSubCategoryName = subCategoryName.lowercase()
+        val subCategoryId = database.push().key
+        if (subCategoryId != null) {
+            database.child(subCategoryId).setValue(lowerCaseSubCategoryName)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            activity,
+                            "Subategoria salva com sucesso",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        binding.tfAddSubCategoryContent.text?.clear()
+                    } else {
+                        // Falha ao salvar
+                        Toast.makeText(
+                            activity,
+                            "Erro ao salvar Subcategoria. Tente novamente.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    binding.progressBarAddSubCategory.visibility = ProgressBar.GONE
+                }
+                .addOnFailureListener { exception ->
+                    // Caso haja falha em qualquer parte da operação
+                    Toast.makeText(
+                        activity,
+                        "Falha na conexão. Erro: ${exception.localizedMessage}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    binding.progressBarAddSubCategory.visibility = ProgressBar.GONE
+                }
+        } else {
+            Toast.makeText(activity, "Erro ao gerar ID para Subcategoria", Toast.LENGTH_SHORT)
+                .show()
+            binding.progressBarAddSubCategory.visibility = ProgressBar.GONE
+        }
+    }
+
+    private fun updateSubCategoryInFirebase(newSubCategoryName: String) {
+        val lowerCaseSubCategoryName = subCategoryName?.lowercase()
+        val lowerCaseNewSubCategoryName = newSubCategoryName.lowercase()
+
+        database.orderByValue().equalTo(lowerCaseSubCategoryName)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        for (subCategorySnapshot in snapshot.children) {
+                            subCategorySnapshot.ref.setValue(lowerCaseNewSubCategoryName)
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        Toast.makeText(
+                                            activity,
+                                            "Subcategoria atualizada com sucesso",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        binding.tfAddSubCategoryContent.text?.clear()
+                                        requireActivity().supportFragmentManager.popBackStack()
+                                    } else {
+                                        Toast.makeText(
+                                            activity,
+                                            "Erro ao atualizar Subcategoria. Tente novamente.",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                    binding.progressBarAddSubCategory.visibility = ProgressBar.GONE
+                                }
+                                .addOnFailureListener { exception ->
+                                    Toast.makeText(
+                                        activity,
+                                        "Falha na conexão. Erro: ${exception.localizedMessage}",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    binding.progressBarAddSubCategory.visibility = ProgressBar.GONE
+                                }
+                        }
+                    } else {
+                        Toast.makeText(activity, "Subcategoria não encontrada", Toast.LENGTH_SHORT)
+                            .show()
+                        binding.progressBarAddSubCategory.visibility = ProgressBar.GONE
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(
+                        activity,
+                        "Erro ao atualizar Subcategoria. Tente novamente.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    binding.progressBarAddSubCategory.visibility = ProgressBar.GONE
+                }
+            })
     }
 
 
--- app/src/main/java/com/cantina/iflanche/screen/fragments/admin/SubcategoryFragment.kt
+++ app/src/main/java/com/cantina/iflanche/screen/fragments/admin/SubcategoryFragment.kt
@@ -1,25 +1,51 @@
 package com.cantina.iflanche.screen.fragments.admin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import com.cantina.iflanche.R
 import com.cantina.iflanche.databinding.FragmentRegisterSubCategoryBinding
+import com.cantina.iflanche.firebase.LoadCategories
+import com.cantina.iflanche.screen.HomeActivity
 
 
 class SubcategoryFragment : Fragment() {
     private var _binding: FragmentRegisterSubCategoryBinding? = null
     private val binding get() = _binding!!
-    private var selectedSubCategory: String? = null
+    private var subCategories: List<String> = emptyList()
 
+    private var selectedSubCategory: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        subCategories = (activity as? HomeActivity)?.categories ?: emptyList()
+
 
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onResume() {
+        super.onResume()
+        loadSubCategories()
+        setupSubCategoryAdapters(subCategories)
+        binding.tfOptionsSubcategorySelect.setText("")
+        binding.tfDropdownSubcategorySelect.error = null
+        binding.tfOptionsSubcategorySelect.clearFocus()
+        (activity as? HomeActivity)?.setAppBarTitle("Gerenciar Subcategoria")
+
+        binding.root.setOnTouchListener { _, _ ->
+            binding.tfOptionsSubcategorySelect.clearFocus()
+            binding.tfDropdownSubcategorySelect.error = null
+            false
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,11 +53,30 @@ class SubcategoryFragment : Fragment() {
     ): View? {
         _binding = FragmentRegisterSubCategoryBinding.inflate(inflater, container, false)
         val view = binding.root
+        
+        binding.tfOptionsSubcategorySelect.setOnClickListener {
+            binding.tfDropdownSubcategorySelect.error = null
+        }
 
+        loadSubCategories()
+        editSubCategory()
+        goToAddSubCategory()
 
-        loadDropdownItems()
+        return view
+    }
 
+    private fun goToAddSubCategory() {
+        binding.fabAddSubCategory.setOnClickListener {
+            val addCategoryFragment = AddSubCategoryFragment()
 
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, addCategoryFragment)
+            transaction.addToBackStack(AddCategoryFragment::class.java.simpleName)
+            transaction.commit()
+        }
+    }
+
+    private fun editSubCategory() {
         binding.btnEditSubcategory.setOnClickListener {
             val fragment = AddSubCategoryFragment()
 
@@ -44,22 +89,65 @@ class SubcategoryFragment : Fragment() {
 
             // Substitui o fragmento atual pelo fragmento de edição
             requireActivity().supportFragmentManager.beginTransaction()
-                .replace(com.cantina.iflanche.R.id.fragment_container, fragment)
+                .replace(R.id.fragment_container, fragment)
                 .setTransition(FragmentTransaction.TRANSIT_NONE)
                 .addToBackStack(null)
                 .commit()
 
         }
-
-        return view
     }
 
-    private fun loadDropdownItems() {
-        val items = listOf("Item 1", "Item 2", "Item 3") // Substitua com seus itens
-        val adapter =
-            ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, items)
+    private fun loadSubCategories() {
+        LoadCategories.loadSubCategories(
+            callback = { loadedSubCategories ->
+                // Verifica se o fragmento está ainda adicionado e visível
+                if (isAdded && isResumed) {
+                    // Atualiza a UI com a lista de categorias
+                    setupSubCategoryAdapters(loadedSubCategories)
+                }
+            },
+            onError = { errorMessage ->
+                Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        )
+
+    }
+
+    private fun setupSubCategoryAdapters(subcategories: List<String>) {
+        val adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_dropdown_item_1line,
+            subcategories
+        )
         binding.tfOptionsSubcategorySelect.setAdapter(adapter)
+
+        binding.btnEditSubcategory.setOnClickListener {
+            selectedSubCategory = binding.tfOptionsSubcategorySelect.text.toString()
+            if (selectedSubCategory!!.isNotEmpty()) {
+                goToEditSubCategoryScreen(selectedSubCategory!!)
+            } else {
+
+                binding.tfDropdownSubcategorySelect.error = "Selecione uma categoria para editar"
+
+            }
+        }
     }
+
+    private fun goToEditSubCategoryScreen(categoryName: String) {
+        val fragment = AddSubCategoryFragment()
+
+        // Cria um bundle com o nome da categoria
+        val bundle = Bundle()
+        bundle.putString("subCategoryName", categoryName)
+        fragment.arguments = bundle
+
+        // Substitui o fragmento atual pelo fragmento de edição
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
