c6b5a6f18ce4f495b4fc2a1fa3d1a279b7e2e82f
Pedro Gabriel <pedro98gabriel@gmail.com>
2024-11-14 14:03:48 -0300

    Update do Load Subcategories

--- app/src/main/java/com/cantina/iflanche/firebase/LoadCategories.kt
+++ app/src/main/java/com/cantina/iflanche/firebase/LoadCategories.kt
@@ -7,11 +7,14 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 object LoadCategories {
-    private val database: DatabaseReference =
+    private val databaseCaregories: DatabaseReference =
         FirebaseDatabase.getInstance().reference.child("categories")
 
+    private val databaseSubCaregories: DatabaseReference =
+        FirebaseDatabase.getInstance().reference.child("subcategories")
+
     fun loadCategories(callback: (List<String>) -> Unit, onError: (String) -> Unit) {
-        database.addValueEventListener(object : ValueEventListener {
+        databaseCaregories.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val categories = mutableListOf<String>()
 
@@ -26,6 +29,27 @@ object LoadCategories {
 
             override fun onCancelled(error: DatabaseError) {
                 val errorMessage = "Erro ao carregar categorias: ${error.message}"
+                onError(errorMessage)
+            }
+        })
+    }
+
+    fun loadSubCategories(callback: (List<String>) -> Unit, onError: (String) -> Unit) {
+        databaseSubCaregories.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val subCategories = mutableListOf<String>()
+
+                for (categorySnapshot in snapshot.children) {
+                    val subCategory = categorySnapshot.getValue(String::class.java)
+                    subCategory?.let {
+                        subCategories.add(it.replaceFirstChar { it.uppercaseChar() })
+                    }
+                }
+                callback(subCategories)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                val errorMessage = "Erro ao carregar Subcategorias: ${error.message}"
                 onError(errorMessage)
             }
         })
